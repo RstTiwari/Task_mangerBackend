a533240d4ce8c61a41083ccb13c75a7f5a1b59eb
RstTiwari <iamrrt2121@gmail.com>
2022-09-13 23:05:22 +0530

    initalize

---
+++ app/routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+
+import {
+  getPosts,
+  getPost,
+  createPost,
+  updatePost,
+  likePost,
+  deletePost,
+}   from "./controller/taskController.js";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.post("/", createPost);
+router.get("/:id", getPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
+
+export default router;
---
+++ index.js
@@ -0,0 +1,29 @@
+
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import cors from "cors";
+import postRoutes from "./app/routes.js";
+dotenv.config()
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL = process.env.DB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+
