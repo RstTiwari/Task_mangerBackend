91e73fbf582125af12a91926b1ad86105c30a82a
RstTiwari <iamrrt2121@gmail.com>
2022-09-13 23:38:52 +0530

    added posts in routes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/", postRoutes);
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 5000;
