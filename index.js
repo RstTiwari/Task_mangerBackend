77af51a72b3687941d631444a96db0d0cae9a8a9
RstTiwari <iamrrt2121@gmail.com>
2022-09-13 23:22:47 +0530

    .\package-lock.json

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postRoutes);
+app.use("/", postRoutes);
 
 const CONNECTION_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 5000;
