9ed805043c8850d99661cd6ef9aabd8459bf90e2
RstTiwari <iamrrt2121@gmail.com>
2022-09-23 23:39:47 +0530

    vercel

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-import express from "express";
 
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import cors from "cors";
+import postRoutes from "./app/routes.js";
+dotenv.config()
 const app = express();
-const port = 9000;
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
 app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
 });
 
-app.listen(9000, () => {
-  console.log(`Starting Server on Port ${port}`);
-});
+
+
+const CONNECTION_URL = process.env.DB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+
