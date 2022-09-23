8561b3392c898a9bf6b110aed5b40b0baa439ab5
Rst Tiwari <79466131+RstTiwari@users.noreply.github.com>
2022-09-24 00:10:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ import postRoutes from "./app/routes.js";
 dotenv.config()
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(cors());
 
 
 app.use("/", (req, res) => {
@@ -19,16 +19,19 @@ app.use("/", (req, res) => {
 
 
 
-const CONNECTION_URL = process.env.DB_URL;
+// const CONNECTION_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
+   app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
     )
-  )
-  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose
+//   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() =>
+//     app.listen(PORT, () =>
+//       console.log(`Server Running on Port: http://localhost:${PORT}`)
+//     )
+//   )
+//   .catch((error) => console.log(`${error} did not connect`));
 
 
